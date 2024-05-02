0a7c8be960429759a9a706239660f3f4f82e16b7
daniel saltarin castillo <dgsaltarin@gmail.com>
2024-05-02 14:35:41 -0500

    added container list component

---
+++ src/components/containerList.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+function generate(element: React.ReactElement) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
+
+const Demo = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
+
+export default function InteractiveList() {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={6}>
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+          Text only
+        </Typography>
+        <Demo>
+          <List>
+            {generate(
+              <ListItem>
+                <ListItemText primary="Single-line item" />
+              </ListItem>,
+            )}
+          </List>
+        </Demo>
+      </Grid>
+    </Grid>
+  );
+}
