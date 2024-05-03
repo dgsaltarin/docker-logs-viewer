b8a2b4833327bc6d96202f70bceeff8e34647e49
daniel saltarin castillo <dgsaltarin@gmail.com>
2024-05-03 11:13:14 -0500

    added addicional request for qa and log details

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,32 @@
 import axios, { AxiosResponse } from "axios";
 
-const BASE_URL = "https://monitor.farmuhub.co";
+const BASE_URL_DEV = "https://monitor.farmuhub.co";
+const BASE_URL_QA = "https://qa.monitor.farmuhub.co";
 
-export const getContainers = async () => {
+export const getDevContainers = async () => {
   const response: AxiosResponse = await axios.get(
-    `${BASE_URL}/ecs_app/list_containers/`,
+    `${BASE_URL_DEV}/ecs_app/list_containers/`,
+  );
+  return response.data.containers;
+};
+
+export const getQAContainers = async () => {
+  const response: AxiosResponse = await axios.get(
+    `${BASE_URL_QA}/ecs_app/list_containers/`,
+  );
+  return response.data.containers;
+};
+
+export const getDevContainerLogs = async (containerName: string) => {
+  const response: AxiosResponse = await axios.get(
+    `${BASE_URL_DEV}/ecs_app/view_logs/${containerName}`,
+  );
+  return response.data;
+};
+
+export const getQAContainerLogs = async (containerName: string) => {
+  const response: AxiosResponse = await axios.get(
+    `${BASE_URL_QA}/ecs_app/view_logs/${containerName}`,
   );
   return response.data;
 };
