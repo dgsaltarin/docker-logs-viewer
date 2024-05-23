22ca76be2a93c7ac183dd1745e260c2e817992a1
daniel saltarin castillo <dgsaltarin@gmail.com>
2024-05-23 15:02:03 -0500

    implemente login reducer in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import containerSlice from "./slices/containerSlice";
 import navigationSlice from "./slices/navigationSlice";
 import logSlice from "./slices/logSlice";
+import loginSlice from "./slices/loginSlice";
 
 const store = configureStore({
   reducer: {
     container: containerSlice,
     navigation: navigationSlice,
     log: logSlice,
+    login: loginSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
